8310c65bf5714a07822de933f7e17f63ed465cc3
lwsy <1290504514@qq.com>
2021-09-27 20:47:46 +0800

    增加了注释

--- Tree/AVLTree.ts
+++ Tree/AVLTree.ts
@@ -1,6 +1,14 @@
 import { FindTree } from "./FindTree";
 //由于插入可能会破坏二叉树的结构。所以，平衡二叉树(AVLTree)在插入和删除的时候，都会顺便
 class AVLTree extends FindTree {
+  //左旋：为了解决插入之后，高度差大于1的问题。
+  //      A
+  //    B
+  //  D
+  //变换后
+  //     B
+  //   D   A
+  //如果B有右孩子，则挂到A的左边。
   static signleRotateWithLeft(tree: AVLTree) {
     //这里使用了parent去改变指向，也可以使用tree.right.left或者tree.left.left去代表高度差大于1的节点，此时他的parent为tree.right或tree.left。
     if (tree.left) {
@@ -22,6 +30,14 @@ class AVLTree extends FindTree {
       }
     }
   }
+  //右旋：为了解决插入之后，高度差大于1的问题。
+  //  A
+  //    B
+  //      C
+  //变换后
+  //     B
+  //   A   C
+  //如果B有左孩子，则挂到A的右边。
   static signleRotateWithRight(tree: AVLTree) {
     if (tree.right) {
       if (tree.parent) {
@@ -42,8 +58,18 @@ class AVLTree extends FindTree {
       }
     }
   }
+  //    A
+  //      B
+  //    C
+  //第一步变换，以B为中心，进行左旋
+  //    A
+  //      C
+  //        B
+  //第二步变换，以C为中心，进行右旋。
+  //  C
+  //A   B 
   static doubleRotateWithLeft(tree: AVLTree) {
-    if (tree.right && tree.right.left) {
+    if (tree.right) {
       AVLTree.signleRotateWithLeft(tree.right);
       const root = AVLTree.signleRotateWithRight(tree);
       if (root) {
@@ -52,8 +78,18 @@ class AVLTree extends FindTree {
       }
     }
   }
+  //    A
+  //  B
+  //    C
+  //第一步变换，以B为中心，进行右旋
+  //    A
+  //  C
+  //B
+  //第二步变换，以C为中心，进行左旋。
+  //  C
+  //B   A 
   static doubleRotateWithRight(tree: AVLTree) {
-    if (tree.left && tree.left.right) {
+    if (tree.left) {
       AVLTree.signleRotateWithRight(tree.left);
       const root = AVLTree.signleRotateWithLeft(tree);
       if (root) {
@@ -63,8 +99,8 @@ class AVLTree extends FindTree {
     }
   }
 }
+//在FindTree插入后，调用该函数。
 FindTree.afterInsert = (element, tree) => {
-
   const leftHeight = tree.left?.deep || 0;
   const rightHeight = tree.right?.deep || 0;
   if ((leftHeight - rightHeight) > 1 && tree.left) {
